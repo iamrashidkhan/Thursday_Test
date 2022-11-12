6e680c19cb098d1683a6ac2fb0f00be647933083
iamrashidkhan <109840147+iamrashidkhan@users.noreply.github.com>
2022-11-12 11:10:08 +0530

    Add files via upload

---
+++ EmployeeManagementSystem/src/test/java/com/Employee/EmployeeTest.java
@@ -0,0 +1,22 @@
+package com.Employee;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+import org.junit.jupiter.api.AfterEach;
+
+
+class EmployeeTest {
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		System.out.println("salary increased by 10%");
+	}
+
+
+}
